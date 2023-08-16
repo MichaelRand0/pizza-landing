d8115015e4df0daf90d639587a16aa899423f585
MichaelRand0 <mihailstepanov.stepanoff2015@yandex.ru>
2023-08-16 21:17:42 +0300

    :zap: Some logic added

--- js/index.js
+++ js/index.js
@@ -3,6 +3,29 @@ const adressInput = document.querySelector("[data-input='adress']")
 const phoneInput = document.querySelector("[data-input='phone']")
 const form = document.getElementById("form")
 const modal = document.getElementById("modal")
+const burgerBtn = document.getElementById('burger_btn')
+const burgerMenu = document.getElementById('burger_menu')
+const picture = document.getElementById('picture')
+const pictureImg = picture.querySelector('.picture__img')
+const pictureTitle = picture.querySelector('.picture__title')
+const pizzaBtns = document.querySelectorAll('.shop__img-btn')
+
+picture.addEventListener('click', e => e.target.classList.remove('picture_active'))
+
+pizzaBtns.forEach(pizza => {
+  pizza.addEventListener('click', (e) => {
+    const title = pizza.closest('.shop__info').querySelector('.shop__subtitle').textContent
+    const img = pizza.querySelector('.shop__img').getAttribute('src')
+    pictureImg.setAttribute('src', img)
+    pictureTitle.textContent = title
+    picture.classList.add('picture_active')
+  })
+})
+
+burgerBtn.addEventListener('click', () => {
+  burgerBtn.classList.toggle('burger_active')
+  burgerMenu.classList.toggle('header__nav-mobile_active')
+})
 
 modal.addEventListener("click", () => {
   modal.classList.remove("modal_active")
